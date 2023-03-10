16530149843f8fcd367b581ee23785c0ff852d09
DerikOmond <derik.omond@gmail.com>
2023-03-10 10:26:48 +0000

    New Project
    Project can process and output text in C# code.

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+    public static void Main(System.String[] args)
+    {
+            Console.WriteLine("Hello World!!!");
+    }
+
+    }
+
+}
