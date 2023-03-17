437b00526bec575342fc944107c5d26d575b22e4
DerikOmond <derik.omond@gmail.com>
2023-03-17 10:17:20 +0000

    Final major changes
    Updating marks if fixed.
    Table of contents formatted.
    Mean, max and min marks can be found.

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -6,23 +6,21 @@ namespace ConsoleApp3
     {
         public static List<Student> studentList = new List<Student>
         {
-            new Student("Thato", "Dior", Student.SetMark(58), Student.SetGrade(58)),
-            new Student("Pemphero", "Zima", Student.SetMark(49), Student.SetGrade(49)),
-            new Student("Jin", "Lane", Student.SetMark(43), Student.SetGrade(43)),
-            new Student("Wei", "Delacroix", Student.SetMark(76), Student.SetGrade(76)),
-            new Student("Claude", "Deniau", Student.SetMark(57), Student.SetGrade(57)),
-            new Student("Munashe", "Joubert", Student.SetMark(33), Student.SetGrade(33)),
-            new Student("Léonce", "Isaev", Student.SetMark(13), Student.SetGrade(13)),
-            new Student("Jawdat", "Fosse", Student.SetMark(82), Student.SetGrade(82)),
-            new Student("Guanyu", "Chauvin", Student.SetMark(91), Student.SetGrade(91)),
-            new Student("Mphatso", "Lémieux", Student.SetMark(63), Student.SetGrade(63))
+            new Student("Thato", "Dior", Student.SetMark(58), Student.SetGrade(Student.SetMark(58))),
+            new Student("Pemphero", "Zima", Student.SetMark(49), Student.SetGrade(Student.SetMark(49))),
+            new Student("Jin", "Lane", Student.SetMark(43), Student.SetGrade(Student.SetMark(43))),
+            new Student("Wei", "Delacroix", Student.SetMark(76), Student.SetGrade(Student.SetMark(76))),
+            new Student("Claude", "Deniau", Student.SetMark(57), Student.SetGrade(Student.SetMark(57))),
+            new Student("Munashe", "Joubert", Student.SetMark(33), Student.SetGrade(Student.SetMark(33))),
+            new Student("Léonce", "Isaev", Student.SetMark(13), Student.SetGrade(Student.SetMark(13))),
+            new Student("Jawdat", "Fosse", Student.SetMark(82), Student.SetGrade(Student.SetMark(82))),
+            new Student("Guanyu", "Chauvin", Student.SetMark(91), Student.SetGrade(Student.SetMark(91))),
+            new Student("Mphatso", "Lémieux", Student.SetMark(63), Student.SetGrade(Student.SetMark(63)))
         };
 
         public static void Main(string[] args) 
         {
             Student.Intro();
-
-            Student.SelectRoute(Student.option, studentList);
         }
     }
 
--- ConsoleApp3/ConsoleApp3/Student.cs
+++ ConsoleApp3/ConsoleApp3/Student.cs
@@ -14,11 +14,11 @@ namespace ConsoleApp3
         private string firstName;
         private string lastName;
         private int mark;
-        private char grade;
+        private char? grade;
 
         public static int option;
 
-         public Student(String firstName, String lastName, int mark, char grade)
+         public Student(String firstName, String lastName, int mark, char? grade)
         {
             this.firstName = firstName;
             this.lastName = lastName;
@@ -28,11 +28,14 @@ namespace ConsoleApp3
 
         public static void Intro()
         {
-
             DisplayHeader();
-            DisplayMenu();
-
-            Console.ReadLine();
+            do
+            {
+                DisplayMenu();
+                SelectRoute(option, Program.studentList);
+            }
+            while (true);
+            
         }
 
         public static void SelectRoute(int option, List<Student> list)
@@ -50,16 +53,28 @@ namespace ConsoleApp3
             }
             else if (option == 3)
             {
-                //OutputMarks();
+                DisplayStudent(list);
             }
             else if (option == 4)
             {
-                //OutputStats();
+                OutputStats(list);
             }
             else if (option == 5)
             {
-                //OutputGradeProfile();
+                DisplayGradeProfile(list, 'A');
+                DisplayGradeProfile(list, 'B');
+                DisplayGradeProfile(list, 'C');
+                DisplayGradeProfile(list, 'D');
+                DisplayGradeProfile(list, 'E');
+                DisplayGradeProfile(list, 'F');
             }
+            else if(option == 6)
+            {
+                Console.WriteLine("Goodbye...");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+           
         }
 
         private static void DisplayHeader()
@@ -79,32 +94,35 @@ Press enter to begin...");
         {
             Console.Write(@"
 MENU:
-1. Input Marks
+1. Update Marks
 2. Add Student
 3. Output Marks
 4. Output Stats
 5. Output Grade Profile
-5. Exit
-Enter the number corrsoponding the the options shown > ");
+6. Exit
+
+Enter the number corrsoponding the the options shown.");
+            Console.Write("\n> ");
             option = Int16.Parse(Console.ReadLine());
-            Console.WriteLine(option);
+            Console.WriteLine();
         }
 
         public static void DisplayStudent(List<Student> list)
         {
-            Console.WriteLine("{0, -5} {1, -14} {2, -14} {3, 3} {4,3}", "Index", 
+            Console.WriteLine();
+            Console.WriteLine("{0, -5} {1, -14} {2, -14} {3, -7} {4,-4}", "Index", 
                 "First Name", "Last Name", "Marks", "Grade");
             foreach (var student in list) 
             {
-                Console.WriteLine("{0, -5} {1, -14} {2, -14} {3, 3} {4, 4}", list.IndexOf(student) + 1,
+                Console.WriteLine("{0, -5} {1, -14} {2, -14} {3, -7} {4, -4}", list.IndexOf(student) + 1,
                     student.firstName, student.lastName, student.mark, student.grade);
             }
-            Console.ReadLine();
+            Console.WriteLine();
         }
 
         public static void UpdateMark(List<Student> list)
         {
-            Console.Write("Select the index of the student you would" +
+            Console.Write("Select the index of the student you would " +
                 "like to change the mark of.\n> ");
             int studentIndex = int.Parse(Console.ReadLine()) - 1;
 
@@ -116,11 +134,13 @@ Enter the number corrsoponding the the options shown > ");
 
             Student selectedStudent = list[studentIndex];
 
-            Console.WriteLine($"You have chosen {selectedStudent.firstName}" +
+            Console.WriteLine($"\nYou have chosen {selectedStudent.firstName}" +
                 $" {selectedStudent.lastName} who currently has {selectedStudent.mark}.");
 
-            Console.WriteLine("Enter the new mark for the student.\n> ");
+            Console.Write("\nEnter the new mark for the student.\n> ");
             selectedStudent.mark = int.Parse(Console.ReadLine());
+
+            selectedStudent.grade = SetGrade(selectedStudent.mark);
 
             DisplayStudent(list);
         }
@@ -185,6 +205,78 @@ Enter the number corrsoponding the the options shown > ");
             }
             while (repeat);
 
+        }
+
+        private static void OutputStats(List<Student> list)
+        {
+            Console.WriteLine($"Mean mark: {CalculateMean(list)}\n" +
+                $"Lowest Mark: {FindMinimum(list)}\n" +
+                $"Highest Mark: {FindMaximum(list)}\n");
+        }
+
+        private static double CalculateMean(List<Student> list)
+        {
+            int total = 0;
+
+            foreach (var student in list)
+            {
+                total += student.mark;
+            }
+
+            double mean = total / list.Count;
+
+            return mean;
+        }
+
+        private static int FindMinimum(List<Student> list)
+        {
+            int lowestMark = 101;
+
+            foreach (var student in list) 
+            {
+                if (student.mark < lowestMark)
+                {
+                    lowestMark = student.mark;
+                }
+            }
+
+            return lowestMark;
+        }
+
+        private static int FindMaximum(List<Student> list)
+        {
+            int highestMark = -1;
+
+            foreach(var student in list)
+            {
+                if (student.mark > highestMark)
+                {
+                    highestMark = student.mark;
+                }
+            }
+
+            return highestMark;
+        }
+
+        private static void DisplayGradeProfile(List<Student> list, char gradeValue)
+        {
+            Console.WriteLine($"{FindProfile(list, gradeValue)}% of students achieved a grade {gradeValue}.");
+        }
+
+        private static double FindProfile(List<Student> list, char gradeValue)
+        {
+            int count = 0;
+
+            foreach(var student in list)
+            {
+                if(student.grade == gradeValue)
+                {
+                    count++;
+                }
+            }
+            
+            double percentage = ((double)count / list.Count) * 100;
+            return percentage;
         }
     }
 }
