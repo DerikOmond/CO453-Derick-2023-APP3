4c7971f39b4effb4b02eebae42d82de554a26ba8
DerikOmond <derik.omond@gmail.com>
2023-03-17 00:19:30 +0000

    List moved
    List becoming an attribute of the Program class.

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -4,22 +4,22 @@ namespace ConsoleApp3
 {
     class Program
     {
+        public static List<Student> studentList = new List<Student>
+        {
+            new Student("Thato", "Dior", Student.SetMark(58), Student.SetGrade(58)),
+            new Student("Pemphero", "Zima", Student.SetMark(49), Student.SetGrade(49)),
+            new Student("Jin", "Lane", Student.SetMark(43), Student.SetGrade(43)),
+            new Student("Wei", "Delacroix", Student.SetMark(76), Student.SetGrade(76)),
+            new Student("Claude", "Deniau", Student.SetMark(57), Student.SetGrade(57)),
+            new Student("Munashe", "Joubert", Student.SetMark(33), Student.SetGrade(33)),
+            new Student("Léonce", "Isaev", Student.SetMark(13), Student.SetGrade(13)),
+            new Student("Jawdat", "Fosse", Student.SetMark(82), Student.SetGrade(82)),
+            new Student("Guanyu", "Chauvin", Student.SetMark(91), Student.SetGrade(91)),
+            new Student("Mphatso", "Lémieux", Student.SetMark(63), Student.SetGrade(63))
+        };
+
         public static void Main(string[] args) 
         {
-            List<Student> studentList = new List<Student>
-            {
-                new Student("Thato", "Dior", 58, 'C'),
-                new Student("Pemphero", "Zima", 49, 'D'),
-                new Student("Jin", "Lane", 43, 'D'),
-                new Student("Wei", "Delacroix", 76, 'A'),
-                new Student("Claude", "Deniau", 57, 'C'),
-                new Student("Munashe", "Joubert", 33, 'F'),
-                new Student("Léonce", "Isaev", 13, 'F'),
-                new Student("Jawdat", "Fosse", 82, 'A'),
-                new Student("Guanyu", "Chauvin", 91, 'A'),
-                new Student("Mphatso", "Lémieux", 63, 'B')
-            };
-
             Student.Intro();
 
             Student.SelectRoute(Student.option, studentList);
